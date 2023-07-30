06acfd225d5dbad8ab537b203682a734801e7771
Devvaru <syeraru@gmail.com>
2023-07-29 19:55:05 -0600

    Rename files and cleanup

--- 3_queries_group-by/3_all_cohorts_with_18.sql
+++ 3_queries_group-by/3_all_cohorts_with_18.sql
@@ -1,5 +1,5 @@
 -- return all cohorts with 18 or more students
-SELECT cohorts.name as cohort_name, count(students.*) AS student_count
+SELECT cohorts.name AS cohort_name, count(students.*) AS student_count
 FROM cohorts
 JOIN students ON cohorts.id = cohort_id
 GROUP BY cohorts.name
